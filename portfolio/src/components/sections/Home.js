ecb754849e10ad596582e82261c640c6150dae00
Gwean <georgina.mondino1@gmail.com>
2022-05-16 15:39:34 -0300

    Imagenes e ilustraciones

--- portfolio/src/components/sections/Home.js
+++ portfolio/src/components/sections/Home.js
@@ -1,9 +1,24 @@
-import '../styles/Home.css'
+import '../../styles/Home.css'
+import Foto from '../../assets/Yo.png'
 
 function Home(){
     return(
         <div className='Home'>
+           <div className='Left'>
+           <img alt='Una foto mia' src= '../../assets/Yo.png'></img>
            
+           </div>
+           <div className='Right'>
+               <h2 style={{fontSize:'50px', marginLeft:0 }}>
+                HOLA,
+               </h2>
+               <p>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur risus metus, varius eu sapien non, gravida scelerisque felis. Ut sed egestas mi, sit amet vestibulum orci. Morbi rutrum vehicula arcu id egestas. Nunc lobortis pellentesque justo, quis fringilla eros accumsan eget. Aliquam est odio, volutpat ut urna eu, faucibus sodales diam. 
+               </p>
+               <button className='Button'>
+                    Projects
+               </button>
+           </div>
         </div>
     );
 }

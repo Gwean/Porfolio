ad2e7a1fd8d563e1f2ae17481200cd1abb970eae
Gwean <georgina.mondino1@gmail.com>
2022-05-16 14:44:30 -0300

    Sections moved into new folder

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './components/sections/Home.js';
 import Nav from './components/Nav.js';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Home/>
     </div>
   );
 }

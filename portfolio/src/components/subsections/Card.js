eb98ea1de463809937110638dfc10d4f394d72f3
Gwean <georgina.mondino1@gmail.com>
2022-05-30 12:20:03 -0300

    Card bonita!!!!!

--- portfolio/src/components/subsections/Card.js
+++ portfolio/src/components/subsections/Card.js
@@ -5,15 +5,22 @@ import '../../styles/Card.css';
 function Card(){
     return(
         <div className="Card " >
-            <img className=" image1 " src={Ilustracion}/>
-            <Flag className="image2"/>
-            <p style={{alignSelf : 'flex-end', marginRight: '5%'}}>June 2021</p>
-           
-                <h3 className="CardLeft">Project Name</h3>
-                    <ul className="CardLeft" style={{margin: '0% 10% 5%'}}>
+            <button className="CardButton">
+                <img className=" image1 " src={Ilustracion}/>
+                    <div >
+                        <Flag className="image2"/>
+                        <p className="imageText">GD</p>
+                    </div>
+                <p style={{alignSelf : 'flex-end', marginRight: '5%', marginBottom : '0%'}}>June 2021</p>
+                <div style={{display : 'flex', alignItems: 'center', width : '100%'}}>  
+                    <h3 className="CardLeft">Project Name </h3>
+                    <p className="SideText">- UI and Logo Design</p>
+                </div>
+                    <ul className="CardLeft" style={{margin: '0% 10% 5%' }}>
                         <li style={{textAlign : 'left'}}>Adobe Illustrator</li>
                         <li style={{textAlign : 'left'}}>Figma</li>
                     </ul>
+            </button>
         </div>
     );
 }
